0a6c646fad5eb4d6e1c0f5f7a0553cfb4de75958
Luis Vadillo <luvar25@comunidad.unam.mx>
2025-01-08 20:51:18 -0600

    Realización de Practica 1 y Vocales y Consonantes (incompleta) (#1)
    * Clase del 7 de Enero.
    
    * Clase del 8 de Enero, Practica 1 Generar Numeros Pares Terminada y Programa Contar Vocales creado pero no terminado.
    
    * Clase del 8 de Enero confirmación, Practica 1 Generar Numeros Pares Terminada y Programa Contar Vocales creado pero no terminado.

---
+++ ContarVocSon.c
@@ -0,0 +1,50 @@
+//
+// Created by YO on 08/01/2025.
+//
+#include <stdio.h>
+
+int countVowels(char* phrase){
+
+  int vocales = 0;
+
+  for(int i = 0; phrase[i] != '\0'; i++) {
+    vocales++;
+  }
+  return vocales;
+
+ }
+
+int countConsonants(char* phrase){
+
+  int consonantes = 0;
+
+  for (int i = 0; phrase[i] != '\0'; i++) {
+    consonantes++;
+  }
+  return consonantes;
+}
+
+void getInput(char* phrase){
+
+
+  if (phrase == NULL){
+    printf ("La frase está vacía");
+  }
+  else{
+    countVowels(phrase);
+    countConsonants(phrase);
+  }
+}
+
+int main(){
+  char* phrase;
+  int vocales,consonantes;
+  scanf ("%s", &phrase);
+
+  getInput(phrase);
+
+
+  printf ("El numero de vocales es %d y el numero de consonantes es %d",vocales,consonantes);
+
+  return 0;
+}
---
+++ HolaMundo.c
@@ -0,0 +1,10 @@
+//
+// Created by YO on 07/01/2025.
+//
+#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
+
