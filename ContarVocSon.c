39d48a21b1f951409341479f4777b0640fc21cb0
Luvar25r <luvar25@comunidad.unam.mx>
2025-01-13 21:32:48 -0600

    Practica 3

--- ContarVocSon.c
+++ ContarVocSon.c
@@ -3,8 +3,14 @@
 //
 #include <stdio.h>
 #include <string.h>
+
+//10 de Enero,
+//1.- Convertir capacidad de palabras a frases
+//2.- Corregir conteo de consonantes
+
 char phrase[100];
 char vocales[5]={'a','e','i','o','u'};
+//nO SE BORRO
 
 int countVowels(char *phrase) {
     int numvocales = 0;
@@ -28,43 +34,38 @@ int countVowels(char *phrase) {
 
 int countConsonants(char *phrase) {
     int numconsonantes = 0;
-
-
+    int flagvocal=0;
     //Primero contar letra por letra y despues comprobar si es vocal
-    int esconsonante=1;
-    for (int i = 0;i<strlen(phrase); i++) { //Cuenta letra por letra
-        for (int j=0;j<=5 && numconsonantes==i;j++) { //Cuenta vocaleS
-                if (phrase[i]!=vocales[j]) {
-                    numconsonantes++;
+    for (int i = 0;i<strlen(phrase) && phrase[i]!= "/0"; i++) { //Cuenta letra por letra
+        int flagconsonante=0;
+        for (int j=0;j<=5 && flagvocal==0 && flagconsonante==0;j++) { //Cuenta vocaleS
+                if (phrase[i]==vocales[j]) {
+                    flagvocal=1;
                 }
+                else {
+                    numconsonantes++;
+                    flagconsonante++;
+                }
+
+
         }
     }
     return numconsonantes;
 }
 
 void getInput() {
-
     scanf("%s", &phrase);
-
     if (phrase == NULL) {
         printf("La frase está vacía");
     }
     else {
         countVowels(phrase);
         countConsonants(phrase);
+        printf("El numero de vocales es %d y el numero de consonantes es %d",countVowels(phrase),countConsonants(phrase));
     }
 }
 
 int main() {
-
-
     getInput();
-
-    int numvocales=countVowels(phrase);
-    int numconsonantes=countConsonants(phrase);
-
-
-    printf("El numero de vocales es %d y el numero de consonantes es %d", numvocales,numconsonantes);
-
     return 0;
 }
